78769fce65931b4e6b41d1d6e9bca69be2c1885d
Olivier Verdier <olivier.verdier@gmail.com>
2017-10-12 15:39:03 +0200

    add more tests
    translation is computed wrong for now

--- test_group.py
+++ test_group.py
@@ -1,9 +1,25 @@
-import pytest
-from pytest import approx
 import group
+import numpy as np
+import numpy.testing as npt
+import pytest
 
 def test_identity():
-    assert approx(group.exponential(group.zero_velocity)) == group.identity
+    computed = group.exponential(group.zero_velocity)
+    expected = group.identity
+    for i in range(2):
+        assert pytest.approx(computed[i]) == expected[i]
+
+def test_rotation():
+    inf = (0, (np.pi, 0, 0))
+    element = group.exponential(inf)[1]
+    npt.assert_allclose(element[:-1,-1], 0)
+    assert pytest.approx(group.get_rotation(element)) == -np.identity(2)
+
+def test_translation():
+    trans = (0, (0, 1., 0))
+    element = group.exponential(trans)[1]
+    npt.assert_allclose(element[:-1,-1], np.array([1.,0]))
+    assert pytest.approx(group.get_rotation(element)) == np.identity(2)
 
 def test_sinc():
     assert pytest.approx(group.sinc(np.pi)) == 0
