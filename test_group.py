fbf0df4388391c01c903017d6a9338b3b4498fd1
Olivier Verdier <olivier.verdier@gmail.com>
2017-10-13 15:28:26 +0200

    add exponential test
    the exponential formula is compared with the matrix exponential

--- test_group.py
+++ test_group.py
@@ -1,6 +1,7 @@
 import group
 import numpy as np
 import numpy.testing as npt
+import scipy.linalg as sl
 import pytest
 
 def test_identity():
@@ -20,6 +21,20 @@ def test_translation():
     element = group.exponential(trans)
     npt.assert_allclose(group.get_rigid(element)[:-1,-1], np.array([1.,0]))
     assert pytest.approx(group.get_rotation(element)) == np.identity(2)
+
+def test_exponential():
+    vec = np.random.randn(3)
+    theta = vec[0]
+    trans = vec[1:]
+    velocity = (0., vec)
+    velocity_matrix = np.zeros([3,3])
+    velocity_matrix[1,0] = theta
+    velocity_matrix[0,1] = -theta
+    velocity_matrix[:2,-1] = trans
+    expected = sl.expm(velocity_matrix)
+    computed = group.get_rigid(group.exponential((0, (theta, trans[0], trans[1]))))
+    assert pytest.approx(computed) == expected
+
 
 def test_sinc():
     assert pytest.approx(group.sinc(np.pi)) == 0
