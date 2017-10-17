bb716d92a2d51fd8bd6a431d7cbed36d7dfb13a2
Olivier Verdier <olivier.verdier@gmail.com>
2017-10-17 15:11:59 +0200

    WIP: rewrite of group module
    Tests outside group module do not pass yet

--- group.py
+++ group.py
@@ -1,8 +1,60 @@
 import numpy as np
 
-zero_velocity = (0, (0, 0, 0))
+class Group():
+    pass
 
-identity = (1., np.identity(3))
+class ContinuousGroup(Group):
+    pass
+
+class Displacement(ContinuousGroup):
+    zero_velocity = (0, 0, 0)
+    identity = np.identity(3)
+
+    @classmethod
+    def exponential(self, velocity):
+        angle, tx, ty = velocity
+        trans = np.array([tx, ty])
+        rot = rotation_matrix(angle)
+        rot2 = rotation_matrix(angle/2)
+        coeff = sinc(angle/2)
+        rotated_trans = np.dot(rot2, trans)
+        matrix = np.zeros([3,3])
+        matrix[:2,:2] = rot
+        matrix[-1,-1] = 1.
+        matrix[:-1, -1] = coeff * rotated_trans
+        return matrix
+
+    @classmethod
+    def get_translation(self, matrix):
+        return matrix[:-1,-1]
+
+    @classmethod
+    def get_rotation(self, group_element):
+        return group_element[:2,:2]
+
+class Scaling(ContinuousGroup):
+    zero_velocity = 0
+    identity = 1.
+
+    @classmethod
+    def exponential(self, velocity):
+        return np.exp(velocity)
+
+def make_product(G1, G2):
+    class Product(ContinuousGroup):
+        zero_velocity = (G1.zero_velocity, G2.zero_velocity)
+        identity = (G1.identity, G2.identity)
+
+        @classmethod
+        def exponential(self, velocity):
+            v1, v2 = velocity
+            return (G1.exponential(v1), G2.exponential(v2))
+    return Product
+
+
+
+ScaleDisplacement = make_product(Scaling, Displacement)
+
 
 def sinc(x):
     return np.sinc(x/np.pi)
@@ -12,33 +64,4 @@ def rotation_matrix(angle):
     s = np.sin(angle)
     return np.array([[c, s],[-s,c]]).T
 
-def exponential(infinitesimal):
-    scale, (angle, tx, ty) = infinitesimal
-    trans = np.array([tx, ty])
-    rot = rotation_matrix(angle)
-    rot2 = rotation_matrix(angle/2)
-    coeff = sinc(angle/2)
-    rotated_trans = np.dot(rot2, trans)
-    matrix = np.zeros([3,3])
-    matrix[:2,:2] = rot
-    matrix[-1,-1] = 1.
-    matrix[:-1, -1] = coeff * rotated_trans
-    return (np.exp(scale), matrix)
 
-def get_rotation(group_element):
-    return group_element[1][:2,:2]
-
-def get_rigid(group_element):
-    return group_element[1]
-
-def get_scale(group_element):
-    return group_element[0]
-
-def create_signed_element(sign, group_element):
-    return (sign, group_element)
-
-def get_sign(signed_group_element):
-    return signed_group_element[0]
-
-def get_group_element(signed_group_element):
-    return signed_group_element[1]
--- test_group.py
+++ test_group.py
@@ -5,24 +5,28 @@ import scipy.linalg as sl
 import pytest
 
 def test_identity():
-    computed = group.exponential(group.zero_velocity)
-    expected = group.identity
+    g = group.Displacement()
+    computed = g.exponential(g.zero_velocity)
+    expected = g.identity
     for i in range(2):
         assert pytest.approx(computed[i]) == expected[i]
 
 def test_rotation():
-    inf = (0, (np.pi, 0, 0))
-    element = group.exponential(inf)
-    npt.assert_allclose(group.get_rigid(element)[:-1,-1], 0)
-    assert pytest.approx(group.get_rotation(element)) == -np.identity(2)
+    g = group.Displacement()
+    inf = (np.pi, 0, 0)
+    element = g.exponential(inf)
+    npt.assert_allclose(g.get_translation(element), 0)
+    assert pytest.approx(g.get_rotation(element)) == -np.identity(2)
 
 def test_translation():
-    trans = (0, (0, 1., 0))
-    element = group.exponential(trans)
-    npt.assert_allclose(group.get_rigid(element)[:-1,-1], np.array([1.,0]))
-    assert pytest.approx(group.get_rotation(element)) == np.identity(2)
+    g = group.Displacement()
+    trans = (0, 1., 0)
+    element = g.exponential(trans)
+    npt.assert_allclose(g.get_translation(element), np.array([1.,0]))
+    assert pytest.approx(g.get_rotation(element)) == np.identity(2)
 
 def test_exponential():
+    g = group.Displacement()
     vec = np.random.randn(3)
     theta = vec[0]
     trans = vec[1:]
@@ -32,7 +36,7 @@ def test_exponential():
     velocity_matrix[0,1] = -theta
     velocity_matrix[:2,-1] = trans
     expected = sl.expm(velocity_matrix)
-    computed = group.get_rigid(group.exponential((0, (theta, trans[0], trans[1]))))
+    computed = g.exponential((theta, trans[0], trans[1]))
     assert pytest.approx(computed) == expected
 
 
