c9f0a058249ae8f29798598f2127b1bdaef59911
Olivier Verdier <olivier.verdier@gmail.com>
2017-10-23 10:53:12 +0200

    add docstrings

--- calibration.py
+++ calibration.py
@@ -16,18 +16,31 @@ def make_discrepancy_from(noisy, product, pairing):
     return discrepancy
 
 def make_loss(original, action, exponential, discrepancy):
+    """
+    Construct a loss function from a group action, and a discrepancy.
+    """
     def loss(velocity):
+        """
+        Loss function from velocity alone.
+        """
         element = exponential(velocity)
         translated = action(element, original)
         return discrepancy(translated)
     return loss
 
 def compute_velocity(original, group, action, discrepancy):
+    """
+    Minimises discrepancy(exp(xi).v),
+    where v is the original.
+    """
     loss = make_loss(original, action, group.exponential, discrepancy)
     best = sopt.minimize(loss, group.zero_velocity)
     return best
 
 def calibrate(original, noisy, group, action, product, pairing):
+    """
+    Main calibration function.
+    """
     discrepancy = make_discrepancy_from(noisy, product, pairing)
     velocity = compute_velocity(original, group, action, discrepancy)
     return velocity
