9b2921665d86bf0d46330cac3ebcb769f6ad501c
Olivier Verdier <olivier.verdier@gmail.com>
2017-10-23 13:05:35 +0200

    remove spaces

--- test_regression.py
+++ test_regression.py
@@ -16,13 +16,13 @@ from accessors import *
 
 def test_solve_regression():
     sigma = 1.0
-    
+
     def kernel(x,y):
         return np.exp(- sum([ (xi - yi) ** 2 for xi, yi in zip(x,y)]) / (sigma ** 2))
-        
+
     def kernel_app_0(x):
         return kernel(x, [0,0])
-       
+
     scale = 0
     tx = 0.0
     ty = 0.0
@@ -40,7 +40,7 @@ def test_solve_regression():
     points2=np.dot(displacement, np.array([0,0,1]))[0:2]
     scale2=get_scale(group_element2)
     alpha2=scale2 * np.dot(group.Displacement.get_rotation(displacement), np.array([1,0]))
-    
+
     space = odl.uniform_discr(
             min_pt =[-10,-10], max_pt=[10,10], shape=[128, 128],
             dtype='float32', interp='linear')
@@ -49,43 +49,38 @@ def test_solve_regression():
     kern = kernel_app_0([mgu for mgu in mg])
     #alpha_ref=[1.0, 0.0]
     #vector_field_ref = space.tangent_bundle.element([kern * hu for hu in alpha_ref]).copy()
-    
+
     alpha0=[0.0, 1.0]
     vector_field0 = space.tangent_bundle.element([kern * hu for hu in alpha0]).copy()
-    
+
     alpha1=[0.0, -1.0]
     vector_field1= space.tangent_bundle.element([kern * hu for hu in alpha1]).copy()
-    
+
     def kernel_app_2(x):
         return kernel(x, points2)
-    
+
     kern2 = kernel_app_2([mgu for mgu in mg])
     vector_field2= space.tangent_bundle.element([kern2 * hu for hu in alpha2]).copy()
-    
+
     sigma0 = 1.0
     sigma1 = 100.0
-    
+
     points_a = np.array([[0.0],[0.0]])
     np_pts_a=len(points_a.T)
     points_b = np.array([[-1.0, 0.0, 1.0],[0.0, 0.0, 0.0]])
     np_pts_b=len(points_b.T)
-    
+
     signed_group_element_list= [signed_group_element0, signed_group_element1, signed_group_element2]
     field_list=[vector_field0, vector_field1, vector_field2]
-    
+
     eval_kernel_a=[[kernel(points_a[:,i], points_a[:,j]) for i in range(np_pts_a)] for j in range(np_pts_a)]
     eval_kernel_b=[[kernel(points_b[:,i], points_b[:,j]) for i in range(np_pts_b)] for j in range(np_pts_b)]
 
     alpha_est0 = reg.solve_regression(signed_group_element_list, field_list, sigma0, sigma1, points_a, eval_kernel_a)
     alpha_est1 = reg.solve_regression(signed_group_element_list, field_list, sigma0, sigma1, points_b, eval_kernel_b)
-    
+
     print('alpha0 expected without regularization [1, 0]')
     print('alpha_est0 ={} '.format(alpha_est0))
-    
+
     print('alpha1 expected without regularization [0, 0, 1, 0, 0, 0]')
     print('alpha_est1 = {}'.format(alpha_est1))
-    
-    
-    
-    
-    
