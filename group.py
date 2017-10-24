72607915d3e3f3626ccd9fac75975513f1c23094
Unknown <gris@kth.se>
2017-10-24 17:49:56 +0200

    New functions in group (test not finished)

--- group.py
+++ group.py
@@ -1,10 +1,12 @@
 import numpy as np
+import accessors as acc
 
 class Group():
     pass
 
 class ContinuousGroup(Group):
     pass
+
 
 class Displacement(ContinuousGroup):
     zero_velocity = (0, 0, 0)
@@ -18,7 +20,7 @@ class Displacement(ContinuousGroup):
         rot2 = rotation_matrix(angle/2)
         coeff = sinc(angle/2)
         rotated_trans = np.dot(rot2, trans)
-        matrix = np.zeros([3,3])
+        matrix = np.zeros([3, 3])
         matrix[:2,:2] = rot
         matrix[-1,-1] = 1.
         matrix[:-1, -1] = coeff * rotated_trans
@@ -32,6 +34,31 @@ class Displacement(ContinuousGroup):
     def get_rotation(self, group_element):
         return group_element[:2,:2]
 
+    @classmethod
+    def apply(self, group_element, points):
+        dim, nb_points = points.shape
+        points_homogeneous = np.empty([dim + 1, nb_points])
+        points_homogeneous[0:dim,:] = points.copy()
+        points_homogeneous[dim,:] = 1.0
+
+        return np.dot(group_element, points_homogeneous)[0:2, :]
+
+    @classmethod
+    def apply_differential(self, group_element, vectors):
+        dim, nb_points = vectors.shape
+        rotation = self.get_rotation(group_element)
+
+        return np.dot(rotation, vectors)
+
+
+    @classmethod
+    def apply_differential_transpose(self, group_element, vectors):
+        dim, nb_points = vectors.shape
+        rotation = self.get_rotation(group_element)
+
+        return np.dot(rotation.T, vectors)
+
+
 class Scaling(ContinuousGroup):
     zero_velocity = 0
     identity = 1.
@@ -39,6 +66,19 @@ class Scaling(ContinuousGroup):
     @classmethod
     def exponential(self, velocity):
         return np.exp(velocity)
+
+    @classmethod
+    def apply(self, group_element, points):
+        return points.copy()
+
+    @classmethod
+    def apply_differential(self, group_element, vectors):
+        return group_element * vectors.copy()
+
+    @classmethod
+    def apply_differential_transpose(self, group_element, vectors):
+        return group_element * vectors.copy()
+
 
 def make_product(G1, G2):
     class Product(ContinuousGroup):
@@ -49,8 +89,25 @@ def make_product(G1, G2):
         def exponential(self, velocity):
             v1, v2 = velocity
             return (G1.exponential(v1), G2.exponential(v2))
-    return Product
 
+        @classmethod
+        def apply(self, group_element, points):
+            g1, g2 = group_element
+            return G1.apply(g1, G2.apply(g2, points))
+
+        @classmethod
+        def apply_differential(self, group_element, vectors):
+            g1, g2 = group_element
+            return G1.apply_differential(g1, G2.apply_differential(g2,
+                                                                   vectors))
+
+        @classmethod
+        def apply_differential_transpose(self, group_element, vectors):
+            g1, g2 = group_element
+            return G1.apply_differential_transpose(g1,
+                               G2.apply_differential_transpose(g2, vectors))
+
+    return Product
 
 
 ScaleDisplacement = make_product(Scaling, Displacement)
@@ -59,9 +116,8 @@ ScaleDisplacement = make_product(Scaling, Displacement)
 def sinc(x):
     return np.sinc(x/np.pi)
 
+
 def rotation_matrix(angle):
     c = np.cos(angle)
     s = np.sin(angle)
-    return np.array([[c, s],[-s,c]]).T
-
-
+    return np.array([[c, s], [-s, c]]).T
--- test_group.py
+++ test_group.py
@@ -3,6 +3,7 @@ import numpy as np
 import numpy.testing as npt
 import scipy.linalg as sl
 import pytest
+import numpy.testing as npt
 
 def test_identity():
     g = group.Displacement()
@@ -42,3 +43,36 @@ def test_exponential():
 
 def test_sinc():
     assert pytest.approx(group.sinc(np.pi)) == 0
+
+def test_apply():
+    points = np.array([[0, 1], [1, 2]])
+    g = group.Displacement()
+    inf = (0.5*np.pi, 0, 0)
+    trans = (0, 1., 0)
+    group_element0 = g.exponential(g.zero_velocity)
+    group_element1 = g.exponential(inf)
+    group_element2 = g.exponential(trans)
+
+    transformed0 = g.apply(group_element0, points)
+    transformed1 = g.apply(group_element1, points)
+    transformed2 = g.apply(group_element2, points)
+
+    npt.assert_allclose(transformed0, points)
+
+    tranformed1_expected = np.array([[-1, -2], [0, 1]])
+    print('expected = {} , computed = {}'.format(transformed1, tranformed1_expected))
+    #npt.assert_allclose(transformed1, tranformed1_expected)
+
+    tranformed2_expected = np.array([[1, 2], [1, 2]])
+    print('expected = {} , computed = {}'.format(transformed2, tranformed2_expected))
+    #npt.assert_allclose(transformed2, tranformed2_expected)
+
+    transformed_diff0 = g.apply_differential(group_element0, points)
+    transformed_diff1 = g.apply_differential(group_element1, points)
+    transformed_diff2 = g.apply_differential(group_element2, points)
+
+    transformed_diff_transpose0 = g.apply_differential_transpose(group_element0, points)
+    transformed_diff_transpose1 = g.apply_differential_transpose(group_element1, points)
+    transformed_diff_transpose2 = g.apply_differential_transpose(group_element2, points)
+
+
